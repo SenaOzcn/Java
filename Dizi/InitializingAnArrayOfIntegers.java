a8395ed70d23032ae8c88796399e07589c9ed2e9
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-31 13:25:40 +0300

    Create InitializingAnArrayOfIntegers.java

---
+++ Dizi/InitializingAnArrayOfIntegers.java
@@ -0,0 +1,19 @@
+/*
+Initialize an int array named numbers with five elements 12, 17, 8, 101, 33 and output it.
+
+Use the provided code template.
+
+
+*/
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        final int[] numbers = {12, 17, 8, 101, 33};
+
+        System.out.println(Arrays.toString(numbers));
+    }
+}
