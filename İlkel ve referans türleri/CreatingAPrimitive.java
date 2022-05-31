9dd339856590b1dc8da5e8daad25f291fea035b3
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-31 12:31:29 +0300

    Create CreatingAPrimitive.java

---
+++ İlkel ve referans türleri/CreatingAPrimitive.java
@@ -0,0 +1,11 @@
+/*
+Create a variable of the primitive type, integer value. Assign the JDK 1.0 release year as a value to the variable. Print the variable to the console.
+*/
+
+public class Main {
+    public static final int RELEASE_YEAR = 1996;
+    
+    public static void main(String[] args) {
+        System.out.println(RELEASE_YEAR);
+    }
+}
