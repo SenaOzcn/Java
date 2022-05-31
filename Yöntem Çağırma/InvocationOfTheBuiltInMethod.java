b51e082c6e41441d94ed770f9819545a80480140
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-31 11:50:55 +0300

    Create InvocationOfTheBuiltInMethod.java

---
+++ Yöntem Çağırma/InvocationOfTheBuiltInMethod.java
@@ -0,0 +1,9 @@
+/*
+There's a method pow() in the Math library that takes two parameters and raises the first one to the power of the second.
+
+Your task is to invoke the method so that it raises a to the power of b.
+
+Write only the invocation of the method, as if a and b are already instantiated.
+*/
+
+Math.pow(a, b);
