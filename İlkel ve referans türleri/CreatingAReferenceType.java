4021f6a7659a6f0399f95085796adc4ae0e0f147
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-31 12:36:04 +0300

    Create CreatingAReferenceType.java

---
+++ İlkel ve referans türleri/CreatingAReferenceType.java
@@ -0,0 +1,12 @@
+/*
+One of the most popular reference types is String. Your task is to create a String variable, assign the value Hello, learner! to it, and print it to the console.
+
+*/
+
+public class Main {
+    public static String task = "Hello, learner!";
+    
+    public static void main(String[] args) {
+        System.out.println(task);
+    }
+}
