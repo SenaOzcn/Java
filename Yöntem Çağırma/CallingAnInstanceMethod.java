1a6d7e098f8366926041e756d5838692bb51e48b
Sena Nur Özcan <1.senaozcan@gmail.com>
2022-05-31 11:48:43 +0300

    Create CallingAnInstanceMethod.java

---
+++ Yöntem Çağırma/CallingAnInstanceMethod.java
@@ -0,0 +1,8 @@
+/*
+You're given a method with the following declaration: public int getAddress(). 
+There is also an already created instance john of the same class where this method is located.
+
+How can you call this method? Write the invocation of the method in one line.
+*/
+
+john.getAddress();
